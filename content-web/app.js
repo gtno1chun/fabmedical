308277cdb27296c3c9b3397bee38fbc01eee8c39
Ubuntu <gtno1chun>
2021-12-07 04:23:15 +0000

    Setup Environment Variables

--- content-web/app.js
+++ content-web/app.js
@@ -6,7 +6,7 @@ const request = require('request');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
-const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
+const contentApiUrl = process.env.CONTENT_API_URL || "http://20.109.172.2:3001";
 
 
 function getSessions(cb) {
